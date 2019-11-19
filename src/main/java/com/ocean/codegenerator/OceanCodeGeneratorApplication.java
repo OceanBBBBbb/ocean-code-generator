96d597dc5b0b77a3ff712b40ab6e0bda76997437
oceanbbbbbb <bhy71090@163.com>
2019-11-19 20:47:01 +0800

    init

---
+++ src/main/java/com/ocean/codegenerator/OceanCodeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.ocean.codegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanCodeGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OceanCodeGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ocean/codegenerator/OceanCodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocean.codegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanCodeGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
